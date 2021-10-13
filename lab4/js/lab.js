f93e945dd85c8bd6abadc876bfc9cc3aba62f63b
Sorantes <91084413+Sorantes@users.noreply.github.com>
2021-10-13 01:10:07 -0700

    This is mainly for Lab4 index and css

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+// Create a box for the game space
+  // Create two vertical rectangles on opposite sides
+    // Give the rectangles vertical movement
+      // Add inputs using the up and down arrow keys
+  // Add a dot to middle of screen
+        // Give the dot X and Y movement
+  // Add reaction movement to the dot
+  // Dot moves in reaction to hitting rectangles
