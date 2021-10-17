9eb099803d4976081195d7fca972f2d4307822d3
Sorantes <91084413+Sorantes@users.noreply.github.com>
2021-10-17 01:36:45 -0700

    Just toying around with Javascript for lab numero cinco

---
+++ lab5/js/lab5.js
@@ -0,0 +1,25 @@
+/**
+ * Author:    Scarlett Orantes <ajorante@ucsc.edu>
+ * Created:   10.15.2021
+ *
+ * (c) Copyright by Blub Corp.
+ */
+
+// Declare and Define Variables
+var make, model, color, year, age, ownIt;
+
+ make = "MINI";
+ model = "Cooper GB";
+ color = "Black";
+ year = 2021;
+ ownIt = true;
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
