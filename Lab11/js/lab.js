420e4d05124729067effc118fbce2494327fd02e
Sorantes <91084413+Sorantes@users.noreply.github.com>
2021-11-08 21:15:59 -0800

    don't mess up, I just finished lab 11

---
+++ Lab11/js/lab.js
@@ -0,0 +1,46 @@
+/**
+ * Author:    Scarlett Orantes
+ * Created:   11.08.2021
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+//create a button that will change #uno into a paragraph upon click a button.
+$("#uno");
+unoEl = $("#uno");
+
+$("#first");
+firstEl = $("#first");
+
+//should allow id to toggle
+ $("#uno").ready(function(){
+   $("#first").click(function(){
+     $("#uno").toggle("x");})});
+
+//find the elements
+$("#dos");
+dosEl = $("#dos");
+
+$("#two");
+twoEl = $("#two");
+
+//should allow id to toggle
+
+$("#dos").ready(function(){
+  $("#two").click(function(){
+    $("#dos").toggle("x");})});
+
+//find the elements
+$("tres");
+tresEl = $("#tres");
+
+$("third");
+thirdEl = $("third");
+
+//should allow id to toggle
+$("#tres").ready(function(){
+  $("#third").click(function(){
+    $("#tres").toggle("x");})});
+
+//change the css styling
+$("uno")
